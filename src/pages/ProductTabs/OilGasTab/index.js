799c071e476d7930730ef969240716e0ac426079
YunhaoZZ <zhangenochyun@outlook.com>
2024-08-20 22:30:50 -0500

    gh page

---
+++ src/pages/ProductTabs/OilGasTab/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OilGasTab = () => {
+   
+    return (
+      <div>
+        <h1>OilGasTab</h1>
+        <p> Insert content + styling. </p>
+      </div>
+    );
+};
+  
+export default OilGasTab;
